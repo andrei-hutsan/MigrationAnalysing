6308f1e8d8a2122bc46313e158acd327af4ee522
andreih <andrei.hutsan@mindsoft.ro>
2025-10-15 12:22:30 +0300

    Upload the project

---
+++ MigrationAnalysing/DataAccess/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MigrationAnalysing.Models;
+
+namespace MigrationAnalysing.DataAccess
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options) { }
+
+        public DbSet<Model1> Model1 => Set<Model1>();
+    }
+}
---
+++ MigrationAnalysing/Models/Model1.cs
@@ -0,0 +1,7 @@
+﻿namespace MigrationAnalysing.Models
+{
+    public class Model1
+    {
+        public int Id { get; set; }
+    }
+}
