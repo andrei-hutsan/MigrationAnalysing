0f197ed009c68571170efc6161eb0c82bca39e86
Daniel Plamadeala <danielp@indsoft.ro>
2025-10-24 14:41:26 +0300

    add precompiled models to dbcontext

--- MigrationAnalysing/Program.cs
+++ MigrationAnalysing/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using MigrationAnalysing;
 using MigrationAnalysing.Benchmark;
 using MigrationAnalysing.DataAccess;
 using System.Diagnostics;
@@ -18,7 +19,10 @@ if (args.Contains("--benchmark"))
 }
 
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("MSSQL_Conn")));
+{
+    options.UseModel(AppDbContextModel.Instance);
+    options.UseSqlServer(builder.Configuration.GetConnectionString("MSSQL_Conn"));
+});
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
