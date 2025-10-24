8f26fcf690607c754e38b9bc8612586a513eec48
andreih <andrei.hutsan@mindsoft.ro>
2025-10-24 12:12:52 +0300

    Add StopWatch on app running

--- MigrationAnalysing/Benchmark/BenchmarkRunner.cs
+++ MigrationAnalysing/Benchmark/BenchmarkRunner.cs
@@ -8,9 +8,11 @@ public static class BenchmarkRunner
 {
     private static int migrationCount = 100;
     private static string resultPath = Path.Combine("artifacts", "results.csv");
-
     public static async Task RunAsync(string mode, string connection)
     {
+        // clear past connections to db
+        Microsoft.Data.SqlClient.SqlConnection.ClearAllPools();
+
         Directory.CreateDirectory("artifacts");
 
         if (!File.Exists(resultPath))
@@ -18,33 +20,29 @@ public static class BenchmarkRunner
 
         Console.WriteLine($"== Running benchmark: {mode} ==");
 
+        long elapsed;
+
         switch (mode.ToLower())
         {
             case "onstartup":
-                await RevertLastNAppliedMigrationsAsync(connection, migrationCount);
                 Console.WriteLine("== Benchmark: NonCompiled Models OnStartup Migration (Cold vs Warm) ==");
+
                 var options = new DbContextOptionsBuilder<AppDbContext>()
                     .UseSqlServer(connection)
                     .Options;
+
                 await ColdVsWarm(options);
+
                 break;
 
-            case "precompiled":
-                Console.WriteLine("== Benchmark: PreCompiled Models OnStartup Migration (Cold vs Warm) ==");
-                var opts = new DbContextOptionsBuilder<AppDbContext>()
-                    .UseSqlServer(connection)
-                    .UseModel(AppDbContextModel.Instance)
-                    .Options;
-                await ColdVsWarm(opts);
-                break;
 
             case "bundle":
-                await RevertLastNAppliedMigrationsAsync(connection, migrationCount);
                 var bundlePath = Path.Combine("artifacts", $"bundle_{migrationCount}.exe");
+
                 if (!File.Exists(bundlePath))
                     throw new FileNotFoundException(bundlePath);
 
-                long elapsed = await MeasureAsync(async () =>
+                elapsed = await MeasureAsync(async () =>
                 {
                     var psi = new ProcessStartInfo
                     {
@@ -59,18 +57,29 @@ public static class BenchmarkRunner
                     string output = await proc.StandardOutput.ReadToEndAsync();
                     string error = await proc.StandardError.ReadToEndAsync();
                     await proc.WaitForExitAsync();
-                    if (!string.IsNullOrWhiteSpace(error))
-                        Console.WriteLine(error);
+                    Console.WriteLine(error);
                 });
-
                 Log("Bundle", migrationCount, elapsed, resultPath);
                 PrintResult("Bundle", elapsed);
+                break;
+
+
+            case "precompiled":
+                Console.WriteLine("== Benchmark: PreCompiled Models OnStartup Migration (Cold vs Warm) ==");
+
+                var opts = new DbContextOptionsBuilder<AppDbContext>()
+                    .UseSqlServer(connection)
+                    .UseModel(AppDbContextModel.Instance)
+                    .Options;
+
+                await ColdVsWarm(opts);
 
                 break;
+
 
             default:
-                Console.WriteLine("Invalid benchmark mode. Use onstartup | precompiled | bundle");
-                break;
+                Console.WriteLine("Invalid benchmark mode. Use migrate | bundle | precompiled");
+                return;
         }
     }
 
@@ -82,11 +91,13 @@ public static class BenchmarkRunner
             await ctx.Database.MigrateAsync();
         });
 
-        long warmElapsed = await MeasureAsync(async () =>
+
+        var warmElapsed = await MeasureAsync(async () =>
         {
             await using var ctx = new AppDbContext(options);
             await ctx.Database.MigrateAsync();
         });
+
 
         Log("OnStartup-Cold", migrationCount, coldElapsed, resultPath);
         Log("OnStartup-Warm", migrationCount, warmElapsed, resultPath);
@@ -116,32 +127,5 @@ public static class BenchmarkRunner
         Console.ForegroundColor = ConsoleColor.Yellow;
         Console.WriteLine($"{method} completed migration in {ms} ms");
         Console.ResetColor();
-    }
-    public static async Task RevertLastNAppliedMigrationsAsync(string connectionString, int lastN)
-    {
-        var options = new DbContextOptionsBuilder<AppDbContext>()
-            .UseSqlServer(connectionString)
-            .Options;
-
-        await using var context = new AppDbContext(options);
-
-        var applied = (await context.Database.GetAppliedMigrationsAsync()).ToList();
-
-        if (applied.Count == 0 || lastN <= 0)
-        {
-            Console.WriteLine("No migrations to revert.");
-            return;
-        }
-        lastN = Math.Min(lastN, applied.Count);
-
-        string targetMigration = lastN == applied.Count
-            ? applied.First() 
-            : applied[applied.Count - lastN - 1];
-       
-        Console.WriteLine($"Reverting last {lastN} migrations to '{targetMigration}'...");
-
-        await context.Database.MigrateAsync(targetMigration);
-
-        Console.WriteLine("Done reverting migrations.");
     }
 }
